7269a9b12121c2b9931b8c4e54ee25141b7ac376
Madrox90 <dvkopylov90@yandex.ru>
2022-06-24 11:44:14 +0300

    Начало модификации

--- Painting/Boat.py
+++ Painting/Boat.py
@@ -14,19 +14,20 @@ Skyblue=(135,206,235)
 Grey=(105,105,105)
 Brown=(139,115,85)
 Yellow=(255,255,0)
+Black1=(0,0,0,65)
+White1=(255,255,255,65)
 
 screen = pygame.display.set_mode((600, 400))
 """Sand,Sea,Sky"""
 polygon(screen, Yellow, [(0,400),(0,300),(600,300),(600,400)])
-
-
 polygon(screen, Blue, [(0,300),(0,200),(600,200),(600,300)])
-"""for _ in range (15"""
-ellipse(screen, Yellow,[0,290,50,20])
-ellipse(screen, Blue,[50,290,50,20])
+for x in range (0,655,110):
+    ellipse(screen, Yellow,[x,295,60,25])
+    ellipse(screen, Blue,[(x+55),285,60,25])
 polygon(screen, Skyblue, [(0,200),(0,0),(600,0),(600,200)])
 """Sun"""
 circle(screen, Yellow, (520,80), 40)
+
 """Umbrella"""
 polygon(screen, Brown, [(100,350),(100,250),(108,250),(108,350)])
 polygon(screen, Red, [(100,250),(100,220),(108,220),(108,250)])
@@ -44,43 +45,63 @@ line(screen, Black,(100,220), (80,250))
 line(screen, Black,(100,220), (60,250))
 line(screen, Black,(100,220), (40,250))
 
-"""Boat"""
-rect(screen,Brown, [320,220 , 200, 30])
-circle(screen,Brown,[320,220],30, draw_bottom_left=True)
-polygon(screen,Brown,[(520,220), (590,220),(520,250)])
-circle(screen,White,(535,231), 8)
-circle(screen,Black,(535,231), 8,2)
+def boat (long, high, size):
+    """ Функция, рисуящая лодку в заданых координатах, размер которой можно кратно задать 
+    1 - обычная лодка, 2 в 2 раза большая итд.
+    """
+    x=long
+    y=high
+    s=size
+    rect(screen,Brown, [x, y, 100*s, 15*s])
+    circle(screen,Brown,[x,y],15*s, draw_bottom_left=True)
+    polygon(screen,Brown,[(x+100*s,y), (x+100*s+35*s,y),(x+100*s,y+15*s)])
+    circle(screen,White,(x+100*s+7*s,y+5*s), 4*s)
+    circle(screen,Black,(x+100*s+7*s,y+5*s), 4*s,s)
+    polygon(screen, Black, [(x+50*s,y),(x+50*s+4*s,y),(x+50*s+4*s,y-50*s),(x+50*s,y-50*s)])
+    polygon(screen, Grey, [(x+50*s+4*s,y-50*s),(x+50*s+30*s,y-50*s+25*s),(x+50*s+30*s-20*s,y-50*s+25*s)])
+    polygon(screen, Black, [(x+50*s+4*s,y-50*s),(x+50*s+30*s,y-50*s+25*s),(x+50*s+30*s-20*s,y-50*s+25*s)], 1)
+    polygon(screen, Grey, [(x+50*s+4*s,y),(x+50*s+30*s,y-50*s+25*s),(x+50*s+30*s-20*s,y-50*s+25*s)])
+    polygon(screen, Black, [(x+50*s+4*s,y),(x+50*s+30*s,y-50*s+25*s),(x+50*s+30*s-20*s,y-50*s+25*s)], 1)
 
-"""Sail"""
-polygon(screen, Black, [(400,220),(408,220),(408,120),(400,120)])
-
-polygon(screen, Grey, [(408,120),(468,170),(428,170)])
-polygon(screen, Black, [(408,120),(468,170),(428,170)], 1)
-
-polygon(screen, Grey, [(408,220),(468,170),(428,170)])
-polygon(screen, Black, [(408,220),(468,170),(428,170)], 1)
 
 """Cloud"""
-circle(screen,White,(140,50), 20)
-circle(screen,Black,(140,50), 20,1)
+def cloud (long, high, size, shape, a):
+    """ Функция, рисуящая облака в заданых координатах, размер которых можно кратно задать (от 0 до 2), 
+    степень сплющенности shape, степень прозрачности где 1 - не прозрачно"""
+    x=long
+    y=high
+    s=size
+    z=shape
+    """if a > 1:
+    ellipse = pygame.Surface((1000,750), pygame.SRCALPHA)
+    White=White1
+    Black=Black1"""
+    ellipse(screen, White,[x,y,30*s+10*z,40*s])
+    ellipse(screen, Black,[x,y,30*s+10*z,40*s],1)
+    
+    ellipse(screen, White,[x-20*s,y+10*s,30*s+10*z,40*s])
+    ellipse(screen, Black,[x-20*s,y+10*s,30*s+10*z,40*s],1)
+    
+    ellipse(screen, White,[x+30*s,y,30*s+10*z,40*s])
+    ellipse(screen, Black,[x+30*s,y,30*s+10*z,40*s],1)
+    
+    ellipse(screen, White,[x+10*s,y+20*s,30*s+10*z,40*s])
+    ellipse(screen, Black,[x+10*s,y+20*s,30*s+10*z,40*s],1)
+    
+    ellipse(screen, White,[x+40*s,y+20*s,30*s+10*z,40*s])
+    ellipse(screen, Black,[x+40*s,y+20*s,30*s+10*z,40*s],1)
+    
+    ellipse(screen, White,[x+60*s,y,30*s+10*z,40*s])
+    ellipse(screen, Black,[x+60*s,y,30*s+10*z,40*s],1)
+    
+    ellipse(screen, White,[x+70*s,y+20*s,30*s+10*z,40*s])
+    ellipse(screen, Black,[x+70*s,y+20*s,30*s+10*z,40*s],1)
 
-circle(screen,White,(120,65), 20)
-circle(screen,Black,(120,65), 20,1)
 
-circle(screen,White,(170,50), 20)
-circle(screen,Black,(170,50), 20,1)
 
-circle(screen,White,(150,70), 20)
-circle(screen,Black,(150,70), 20,1)
-
-circle(screen,White,(180,70), 20)
-circle(screen,Black,(180,70), 20,1)
-
-circle(screen,White,(200,50), 20)
-circle(screen,Black,(200,50), 20,1)
-
-circle(screen,White,(210,69), 20)
-circle(screen,Black,(210,69), 20,1)
+boat(100,200,1)
+boat(300,250,2)
+cloud(250,5,0.5,1,1)
 
 
 
--- Painting/новый 1.py
+++ Painting/новый 1.py
@@ -1,34 +1,142 @@
 import pygame
-from math import pi
 from pygame.draw import *
 
 pygame.init()
 
 FPS = 30
+screen = pygame.display.set_mode((1032, 768))
 
-Black=(0,0,0)
-Red=(255,0,0)
-Blue=(0,0,255)
-White=(255,255,255)
-Skyblue=(135,206,235)
-Grey=(105,105,105)
-Brown=(139,115,85)
-Yellow=(255,255,0)
-
-screen = pygame.display.set_mode((600, 400))
-"""Sand,Sea,Sky"""
-
-arc(screen, Red,[0,295,50,5], 0.35, 2.8, 3)
+"""Во всех фунцкиях х и у --- начальная точка, аналог х0 и у0"""
 
 
+def dy(y1, y0, scale):
+    return int((y1 - y0) * scale)
 
 
+def dx(x1, x0, ko, scale):
+    return int(ko * (x1 - x0) * scale)
 
 
+def chuvak(x, y, orientacia, scale):
+    """чувак
+        Туловище,голова, руки, ноги, ступни """
+    x0 = 330
+    y0 = 215
+    if orientacia == 1:
+        koef_orientacii = 1
+        ko = koef_orientacii
+    else:
+        koef_orientacii = -1
+        ko = koef_orientacii
 
-    
+    ellipse(screen, (167, 147, 172), (x + dx(255, x0, ko, scale), y + dy(230, y0, scale), ko * 150 * scale, 350 * scale))
+    circle(screen, (244, 227, 215), (x + dx(330, x0, ko, scale), y + dy(215, y0, scale)), int(50 * scale))
+    line(screen, (0, 0, 0), (x + dx(285, x0, ko, scale), y + dy(275, y0, scale)), (x + dx(140, x0, ko, scale),
+                                                                                   y + dy(400, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(375, x0, ko, scale), y + dy(275, y0, scale)), (x + dx(500, x0, ko, scale),
+                                                                                   y + dy(400, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(290, x0, ko, scale), y + dy(550, y0, scale)), (x + dx(210, x0, ko, scale),
+                                                                                   y + dy(700, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(368, x0, ko, scale), y + dy(550, y0, scale)), (x + dx(425, x0, ko, scale),
+                                                                                   y + dy(700, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(210, x0, ko, scale), y + dy(700, y0, scale)), (x + dx(130, x0, ko, scale),
+                                                                                   y + dy(710, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(425, x0, ko, scale), y + dy(700, y0, scale)), (x + dx(510, x0, ko, scale),
+                                                                                   y + dy(710, y0, scale)), 1)
 
-pygame.display.update()
+
+def morozhka(x, y, orientacia, scale):
+    """морожка чувака
+        Рожок, левый шарик, правый шарик, вехний шарик"""
+    x0 = 155
+    y0 = 415
+    r = 16
+    if orientacia == 1:
+        koef_orientacii = 1
+        ko = koef_orientacii
+    else:
+        koef_orientacii = -1
+        ko = koef_orientacii
+
+    polygon(screen, (255, 204, 0), [(x + dx(155, x0, ko, scale), y + dy(415, y0, scale)), (x + dx(80, x0, ko, scale),
+                                                                                           y + dy(380, y0, scale)),
+                                    (x + dx(125, x0, ko, scale), y + dy(335, y0, scale))])
+    circle(screen, (85, 0, 0), (x + dx(90, x0, ko, scale), y + dy(365, y0, scale)), int(r * scale))
+    circle(screen, (255, 0, 0), (x + dx(110, x0, ko, scale), y + dy(345, y0, scale)), int(r * scale))
+    circle(screen, (255, 255, 255), (x + dx(85, x0, ko, scale), y + dy(340, y0, scale)), int(r * scale))
+
+
+def chuviha(x, y, orientacia, scale):
+    """Чувиха
+    Туловище, голова, руки, ноги"""
+    x0 = 700
+    y0 = 215
+    if orientacia == 1:
+        koef_orientacii = 1
+        ko = koef_orientacii
+    else:
+        koef_orientacii = -1
+        ko = koef_orientacii
+
+    polygon(screen, (255, 85, 221), [(x + dx(700, x0, ko, scale), y + dy(215, y0, scale)),
+                                     (x + dx(600, x0, ko, scale), y + dy(580, y0, scale)),
+                                     (x + dx(800, x0, ko, scale), y + dy(580, y0, scale))])
+    circle(screen, (244, 227, 215), (x + dx(700, x0, ko, scale), y + dy(215, y0, scale)), int(50 * scale))
+    line(screen, (0, 0, 0), (x + dx(685, x0, ko, scale), y + dy(275, y0, scale)), (x + dx(500, x0, ko, scale),
+                                                                                   y + dy(400, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(715, x0, ko, scale), y + dy(275, y0, scale)),
+         (x + dx(800, x0, ko, scale), y + dy(338, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(800, x0, ko, scale), y + dy(338, y0, scale)),
+         (x + dx(885, x0, ko, scale), y + dy(270, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(670, x0, ko, scale), y + dy(580, y0, scale)),
+         (x + dx(670, x0, ko, scale), y + dy(700, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(730, x0, ko, scale), y + dy(580, y0, scale)),
+         (x + dx(730, x0, ko, scale), y + dy(700, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(670, x0, ko, scale), y + dy(700, y0, scale)),
+         (x + dx(610, x0, ko, scale), y + dy(700, y0, scale)), 1)
+    line(screen, (0, 0, 0), (x + dx(730, x0, ko, scale), y + dy(700, y0, scale)),
+         (x + dx(790, x0, ko, scale), y + dy(705, y0, scale)), 1)
+
+
+def sharik(x, y, orientacia, scale):
+    """Шарик чувихи
+    Палка, треугольник, два круга"""
+    x0 = 880
+    y0 = 300
+    if orientacia == 1:
+        koef_orientacii = 1
+        ko = koef_orientacii
+    else:
+        koef_orientacii = -1
+        ko = koef_orientacii
+
+    line(screen, (0, 0, 0), (x + dx(880, x0, ko, scale), y + dy(300, y0, scale)),
+         (x + dx(900, x0, ko, scale), y + dy(150, y0, scale)), 1)
+    polygon(screen, (255, 0, 0),
+            [(x + dx(900, x0, ko, scale), y + dy(155, y0, scale)), (x + dx(880, x0, ko, scale), y + dy(65, y0, scale)),
+             (x + dx(
+                 950, x0, ko, scale), y + dy(
+                 80, y0, scale))])
+    circle(screen, (255, 0, 0), (x + dx(935, x0, ko, scale), y + dy(70, y0, scale)), int(21 * scale))
+    circle(screen, (255, 0, 0), (x + dx(899, x0, ko, scale), y + dy(60, y0, scale)), int(21 * scale))
+
+
+"""Фон
+Небо, земля"""
+rect(screen, (170, 238, 255), (0, 0, 1032, 384))
+rect(screen, (55, 200, 113), (0, 380, 1032, 384))
+
+chuvak(400, 330, 1, 1)
+chuvak(1032 - 240, 330, -1, 0.5)
+chuviha(425, 330, 1, 0.5)
+chuviha(1032 - 425, 330, -1, 0.5)
+sharik(150, 430, -1, 1 / 1.5)
+morozhka(885, 425, -1, 1 / 1.5)
+
+line(screen, (0, 0, 0), (507, 380), (560, 230), 1)
+morozhka(560, 230, 1, 1.5)
+
+pygame.display.flip()
 clock = pygame.time.Clock()
 finished = False
 
@@ -37,5 +145,3 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-
-pygame.quit()
